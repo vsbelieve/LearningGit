28f939b2cde083e0dca6f5162d095bd2750a2cde
Vishal Singh Balyan <vsbelieve2@gmail.com>
2022-06-05 12:17:51 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+
+public class Sum{
+    
+    public static void main(String[] args ) {
+        int a = 5;
+        int b = 6;
+
+        System.out.println("the sum of a and b is -->" + a+b);
+    }
+}
