5b65042b9c13170bbbea02a54d57a1f469225a58
Vishal Singh Balyan <vsbelieve2@gmail.com>
2022-06-06 15:22:52 +0530

    adding two functions least common ancestor and distance between 2 nodes

--- FindDataTree.java
+++ FindDataTree.java
@@ -71,16 +71,45 @@ public class FindDataTree {
     } // if and if only if the data is found the list will have some data
  
     for (Node child : node.children) {
-      ArrayList<Integer> ptc =(nodeToRootPath(child, data)); // equating list new ptc to the returned list
+      ArrayList<Integer> ptc =(nodeToRootPath(child, data)); // receiveing the list in a new list ptc 
       
       if(ptc.size() > 0 ){
-        ptc.add(node.data);
-        return ptc;
+        ptc.add(node.data); // add the current node to the list if 
+        return ptc; // returnig this so that recursion stops further
        }
     }
      return new ArrayList<>(); // return an empty array list
   }
- 
+  public static int lca(Node node, int d1, int d2) { //least common ancestor code
+    ArrayList<Integer> p1 = nodeToRootPath(node, d1);
+    ArrayList<Integer> p2 = nodeToRootPath(node, d2);
+
+    int i = p1.size() - 1;
+    int j = p2.size() - 1;
+
+    while(i >= 0 && j >= 0 && p1.get(i) == p2.get(j)){
+      i--;
+      j--;
+    }
+
+    return p1.get(i + 1);
+  }
+
+  public static int distanceBetweenNodes(Node node, int d1, int d2){ // uses least common ancestor code to find distance between 2 nodes;
+    ArrayList<Integer> a1 = nodeToRootPath(node, d1);
+    ArrayList<Integer> a2 = nodeToRootPath(node, d2);
+    int i = a1.size()-1;
+    int j = a2.size()-1;
+    while ( i>=0 && j>=0 && a1.get(i) == a2.get(j) ) {
+      i--;
+      j--;
+    }
+    i++;
+    j++;
+
+    return i+j;
+  }
+
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
