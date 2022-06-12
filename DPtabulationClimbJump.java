25b45f04c61718486028f02670621d11fc307f59
Vishal Singh Balyan <vsbelieve2@gmail.com>
2022-06-12 15:36:06 +0530

    VVIP DP Tabulation method alternative to recursion and easy to solve

--- DPtabulationClimbJump.java
+++ DPtabulationClimbJump.java
@@ -15,12 +15,12 @@ public class DPtabulationClimbJump {
     dp[n] =1; // sorta base case- find direction of smaller problem
 
     
-    for (int i = n-1; i >=0; i--) {
+    for (int i = n-1; i >=0; i--) { // here i+j < dp.length is done bcoz we dont want invalid paths where stair at i do not jump out of dp.length
         for (int j = 1; j <= arr[i] && i + j < dp.length; j++) {
             dp[i] +=  dp[i+j]; // this tells the path from i to n via all paths from j so, adding them up
         }
     }
-
+    
     System.out.println(dp[0]);
     }
     
